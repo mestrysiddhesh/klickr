ea88734a54b7838fcfc3c7291d41a436ca003c94
Siddhesh <mestrysiddhesh@yahoo.com>
2016-07-13 11:22:55 +0000

    After adding post button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,8 @@ class PagesController < ApplicationController
   else
     redirect_to root_path, :notice => "User not found"
     end
-    @posts = Post.all.where("user_id= ?", (User.find_by_username(params[:id])))
+    @posts = Post.all.where("user_id= ?", (User.find_by_username(params[:id])).id)
+    @newPost = Post.new
   end
   def explore
     @posts = Post.all
